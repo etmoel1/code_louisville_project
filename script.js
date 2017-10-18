6e93f57ebc39b44cca51f858aa8b132fcb0103b9
Eric Moeller <etmoel1@gmail.com>
2017-10-17 21:58:35 -0400

    Commit 10/17 night

--- script.js
+++ script.js
@@ -4,3 +4,41 @@ jQuery(document).ready(function($){
   $(this).next().toggle("fast").siblings(".content").hide("fast");
     });
 });
+
+var $background = $('<div id="background"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+//An image to overlay
+$background.append($image);
+
+////A caption to overlay
+//$background.append($caption);
+
+//Add overlay
+$("body").append($background);
+
+//Capture the click event on a link to an image
+$("#images a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  //Update overlay with the image linked in the link
+  $image.attr("src", imageLocation);
+  
+  //Show the overlay.
+  $background.show();
+  
+  //Get child's alt attribute and set caption
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+//When overlay is clicked
+$background.click(function(){
+  //Hide the overlay
+  $background.hide();
+});
+
+
+
+
